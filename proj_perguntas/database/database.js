1bbd57c82d655f03ec75389628a180cc1439610a
daniel <danielpaivaborges@outlook.com>
2024-09-04 07:56:31 -0300

    recomecando

--- proj_perguntas/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const conexao = new Sequelize("everos", "everos", "root2024", {
-  host: "mysql30-farm10.kinghost.net",
-  dialect: "mysql"
-});
-
-module.exports = conexao;
