5d7f9aaa252bc828967b0e0daec4573d651967c8
daniel <danielpaivaborges@outlook.com>
2024-09-06 08:59:59 -0300

    primeira do dia

--- complete_node-curso/index.js
+++ complete_node-curso/index.js
@@ -1,1 +1,7 @@
-console.log("oi mundo doido");
+const mission = process.argv[2];
+
+if (mission == "learn") {
+  console.log("Time do write some Node code");
+} else {
+  console.log(`is ${mission} really more fun?`);
+}
