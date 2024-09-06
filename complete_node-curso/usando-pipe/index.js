4582a805aebafc3a68cc11d2532b7222740c2114
daniel <danielpaivaborges@outlook.com>
2024-09-06 15:34:40 -0300

    nova

--- complete_node-curso/usando-pipe/index.js
+++ complete_node-curso/usando-pipe/index.js
@@ -1,17 +1,15 @@
 "use strict";
-const http = require('http')
-const fs = require('fs');
-const { Readable } = require('stream');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    const readableStream = fs.createReadStream('teste.txt')
-    
-    res.writeHead(200, {'content-type': 'text/plain'})
-    readableStream.pipe(res)
+const server = http.createServer((req, res) => {
+  const readableStream = fs.createReadStream("teste.txt");
 
-})
+  res.writeHead(200, { "content-type": "text/plain" });
+  readableStream.pipe(res);
+  //res.write(readableStream)
+});
 
-
-server.listen(3000, ()=>{
-    console.log('servidor ativo')
-})
+server.listen(3000, () => {
+  console.log("servidor ativo");
+});
