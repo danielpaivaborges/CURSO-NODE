d0170ed1f07e58337b40849f1d82f9690b28fc1e
daniel <danielpaivaborges@outlook.com>
2024-09-06 14:48:34 -0300

    remessa da tarde

---
+++ complete_node-curso/projeto-planetas/index.js
@@ -0,0 +1,41 @@
+"use strict";
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const results = [];
+
+//filtrando os dados
+function isHabitalePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+//criando o stream e fazendo um PIPE para redirecionar os dados para o parse
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+      delimiter: ','
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitalePlanet(data)) {
+      results.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(
+      results.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`foram encontrados ${results.length} planetas habitáveis.`);
+    console.log("fim da leitura");
+  });
