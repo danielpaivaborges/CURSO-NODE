1262aa824c427895803345cf65c0bb8fca373e6a
daniel <danielpaivaborges@outlook.com>
2024-09-10 15:48:41 -0300

    inicio do servidor nasa

---
+++ complete_node-curso/nasa/client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ import App from "./App";
+  
+  const Index = () =>  {
+	return (
+	  <div>
+	  </div>
+	);
+  }
+  
+  export default Index;
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
